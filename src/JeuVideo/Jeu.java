22f468d034e782b4669f52105309465717dc71d4
Ikasaidi <ikramsaidi47@gmail.com>
2024-03-27 12:01:17 -0400

    TEST

--- src/JeuVideo/Jeu.java
+++ src/JeuVideo/Jeu.java
@@ -3,24 +3,22 @@ package JeuVideo;
 import java.util.Scanner;
 
 public class Jeu {
-    PNJ[] Boss = {new PNJ("Bowser",(short)10,new Dague((short)40),(short)400),
-                  new PNJ("Ganondorf",(short)500,new Epee((short)400),(short)5000),
-                  new PNJ("Malenia",(short)4000,new Arc((short)6000),(short)10000)
-                };
-    Arme[] armes = {
-            new Arc((short)60),
-            new Epee((short)80),
-            new Masse((short)200),
-            new Dague((short)20),
-            new Arbalete((short)80)
-    };
 
-    PNJ[] Accolyte = {
-            new PNJ("Robin", (short)30, armes[0], (short)120),
-            new PNJ("Hulk", (short)400,armes[2],(short)6000)
-    };
 
     public static void main(String[] args) {
+        PNJ[] boss = {new PNJ("Bowser",(short)40,new Dague((short)40),(short)400),
+                new PNJ("Ganondorf",(short)500,new Epee((short)400),(short)5000),
+                new PNJ("Malenia",(short)1000,new Arc((short)6000),(short)10000)
+        };
+        Arme[] armes = {
+                new Arc((short)60),
+                new Epee((short)80),
+                new Masse((short)200),
+                new Dague((short)20),
+                new Arbalete((short)80)
+        };
+
+
         System.out.println(" Sélectionner votre arme");
         System.out.println(" 1 - Arc");
         System.out.println(" 2 - Épée");
@@ -49,9 +47,45 @@ public class Jeu {
         else{
             System.out.println("Error stupido");
         }
+        System.out.println("Vous avex choisi l'arme :  "+ arme);
 
-        System.out.println(arme);
+        //création d'un hero de niveau 1 // pdv 200
+        Hero perso = new Hero("Saitama",(short) 1,arme,(short) 200,0);
+
+        battreBoss(perso,boss[0]);
+
+
     }
+    public static void battreBoss(Hero perso, Personnage boss){
+
+        // verifie le pdv du boss avant l'attaque System.out.println(boss[0].getPdv());
+        //perso.attaquer(boss[0]);
+        // verifie le pdv du boss apres l'attaque System.out.println(boss[0].getPdv());
+
+        //attaquer le boss et puis attaquer le perso en boucle et verifier si le pdv du boss est rendu
+        // a 0, pour que le match fini
+        while (true){
+            while (perso.getPdv() != 0 && boss.getPdv() != 0){
+                perso.attaquer(boss);
+                if(boss.getPdv() != 0){
+                    boss.attaquer(perso);
+                }
+            }
+            System.out.println(perso + " vs " + boss);
+            // donner de xp au hero si il gagne ou perd
+            if (perso.getPdv() == 0){
+                perso.setXp(boss.getNiveau()*2);
+            }else {
+                perso.setXp(boss.getNiveau()*8);
+                break;
+            }
+            // reprendre leur pdv a la fin
+            boss.setPdv((short) (10* boss.getNiveau()));
+            perso.setPdv((short) (180 + 20 * perso.getNiveau()));
+
+        }
+    }
+
 
 }
 
--- src/JeuVideo/Personnage.java
+++ src/JeuVideo/Personnage.java
@@ -1,5 +1,5 @@
 package JeuVideo;
-
+import java.lang.Math;
 public class Personnage {
     private String nom;
     private short niveau;
@@ -11,5 +11,50 @@ public class Personnage {
         this.niveau = niveau;
         this.arme = arme;
         this.pdv = pdv;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public short getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(short niveau) {
+        this.niveau = niveau;
+    }
+
+    public Arme getArme() {
+        return arme;
+    }
+
+    public void setArme(Arme arme) {
+        this.arme = arme;
+    }
+
+    public short getPdv() {
+        return pdv;
+    }
+
+    public void setPdv(short pdv) {
+        if(pdv < 0){
+            pdv = 0;
+        }
+        this.pdv = pdv;
+    }
+
+    public void attaquer(Personnage victime){
+        //degat infligé = puissance arme * 1.1 exposant le niveau de ton personnage
+        victime.setPdv((short)(victime.getPdv() - this.arme.getDpc() * Math.pow(1.1,this.getNiveau()))) ;
+    }
+
+    @Override
+    public String toString() {
+        return this.nom + "(" + this.niveau + ") avec l'arme : " + this.arme + " pdv restants : " + this.pdv;
     }
 }
